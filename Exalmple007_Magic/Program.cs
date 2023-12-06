a91578b0e97f726b93a7e1eed58ec17d1f4e174c
Sergey <sergeinickit@gmail.com>
2023-12-06 23:54:04 +0300

    Добавил example007

---
+++ Exalmple007_Magic/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+int xa = 1;
+int xb = 1;
+int ya = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
