418840afb90554e598f19c314bc9912130bfca5a
Sergey <sergeinickit@gmail.com>
2023-12-06 21:36:09 +0300

    Добавили файл

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.Readline();
+
+if (username == "Маша");
+{
+    Console.Write("Ура! Это же Маша");
+}
+  else
+{
+    Console.Write("Привет.");
+    Console.Write(username);
+}
+
+
+
+
+
+
+
+
+
