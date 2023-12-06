5c1d0f9ad594276b56b513c68e280f92ee1ba7b3
Sergey <sergeinickit@gmail.com>
2023-12-06 22:50:47 +0300

    Добавил gitignore в корень

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int = 1;
+int = 2;
+int = 6;
+int = 8;
+int = 4;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);
+
+
